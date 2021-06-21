7a87118c9e529783b37c6fcf08abc983a36a1ab4
Leedah8 <ladyk162005@msn.com>
2021-06-21 16:05:53 -0400

    initial commit

--- src/component/todo/todo.js
+++ src/component/todo/todo.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Container, Fab, Icon, withStyles } from "@material-ui/core";
+import Checkbox from '@material-ui/core/Checkbox';
 import { Delete } from "@material-ui/icons";
 import Modal from "../modal";
 import "./todo.css";
@@ -14,15 +15,23 @@ import "./todo.css";
 // const handleClose = () => setShow(false);
 // const handleShow = () => setShow(true);
 
+
+
 const styles = (theme) => ({
   fab: {
     margin: theme.spacing.unit,
   },
 });
 
+
 export function TodoList(props) {
   const { classes } = props;
   const [show, setShow] = useState(false);
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
 
   // const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -30,6 +39,12 @@ export function TodoList(props) {
   return (
     <div>
       <Container>
+      <Checkbox
+        id="chkBox"
+        checked={checked}
+        onChange={handleChange}
+        inputProps={{ 'aria-label': 'primary checkbox' }}
+      />
         <Fab id ="editBtn" color="secondary" aria-label="Edit" className={classes.fab}>
           <Icon onClick={handleShow}>edit_icon</Icon>
           <Modal show={show} />
