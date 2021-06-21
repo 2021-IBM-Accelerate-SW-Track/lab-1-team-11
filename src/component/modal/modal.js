30b2a43f1c36265d9794ee4389dbca50152bd20e
Leedah8 <ladyk162005@msn.com>
2021-06-21 19:16:07 -0400

    commit

--- src/component/modal/modal.js
+++ src/component/modal/modal.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import "./modal.css";
 import { Button } from "@material-ui/core";
+import "./modal.css";
 
 
 const Modal = (props) => {
@@ -17,13 +17,14 @@ const Modal = (props) => {
         <div className="modal-header">
         <input className="modal-title" type="text" name="name"/>
         </div>
-        <div className="modal-body">This is a modal.</div>
+        <div className="modal-body">Edit Item Here</div>
         <div className="modal-footer">
           <Button variant="contained" color="secondary" className="button1" onClick={handleClose}>
             Close
             <Modal close={close} />
           </Button>
-          <Button variant="contained" color="primary" className="button2" onClick={handleClose}>Save Changes
+          <Button variant="contained" color="primary" className="button2" onClick={handleClose}>
+            Save Changes
           <Modal close={close} />
           </Button>
         </div>
