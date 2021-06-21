5834c86ac03d86b56564efe3222aaa5b2334dd06
Leedah8 <ladyk162005@msn.com>
2021-06-21 16:22:13 -0400

    initial commit

--- src/component/todo/todo.js
+++ src/component/todo/todo.js
@@ -1,31 +1,11 @@
 import React, { useState } from "react";
-import { Container, Fab, Icon, withStyles } from "@material-ui/core";
-import Checkbox from '@material-ui/core/Checkbox';
+import { Container, Icon } from "@material-ui/core";
+import Checkbox from "@material-ui/core/Checkbox";
 import { Delete } from "@material-ui/icons";
 import Modal from "../modal";
 import "./todo.css";
 
-
-//We want to eventually import useState in order to
-// track the state of each item for editing purposes
-// Maybe using the code below
-
-// const [showModal, setShow] = useState(false);
-
-// const handleClose = () => setShow(false);
-// const handleShow = () => setShow(true);
-
-
-
-const styles = (theme) => ({
-  fab: {
-    margin: theme.spacing.unit,
-  },
-});
-
-
-export function TodoList(props) {
-  const { classes } = props;
+export default function TodoList() {
   const [show, setShow] = useState(false);
   const [checked, setChecked] = React.useState(true);
 
@@ -39,22 +19,17 @@ export function TodoList(props) {
   return (
     <div>
       <Container>
-      <Checkbox
-        id="chkBox"
-        checked={checked}
-        onChange={handleChange}
-        inputProps={{ 'aria-label': 'primary checkbox' }}
-      />
-        <Fab id ="editBtn" color="secondary" aria-label="Edit" className={classes.fab}>
-          <Icon onClick={handleShow}>edit_icon</Icon>
-          <Modal show={show} />
-        </Fab>
-        <Fab id ="deleteBtn" disabled aria-label="Delete" className={classes.fab}>
-          <Delete />
-        </Fab>
+        <Checkbox
+          checked={checked}
+          onChange={handleChange}
+          inputProps={{ "aria-label": "primary checkbox" }}
+        />
+        <Icon id="editBtn" onClick={handleShow}>
+          edit_icon
+        </Icon>
+        <Modal show={show} />
+        <Delete id="deleteBtn" />
       </Container>
     </div>
   );
 }
-
-export default withStyles(styles)(TodoList);
