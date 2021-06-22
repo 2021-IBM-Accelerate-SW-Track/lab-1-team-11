686eff9b9dc84a732825b7c27d90e06ae0374189
Jwynd1 <57572152+Jwynd1@users.noreply.github.com>
2021-06-22 00:49:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,53 @@ import Header from "./component/header";
 import Input from "./component/input";
 import TodoList from "./component/todo";
 import {Container} from "@material-ui/core"
+import React, { useState } from "react";
 import "./App.css";
+import { nanoid } from "nanoid";
 
-function App() {
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  function deleteT(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  function editT(id, Edited) {
+    const editedTaskList = tasks.map(task => {
+      if (id === task.id) {
+        //
+        return {...task, name: Edited}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+  const taskList = tasks.map(task => (
+    <TodoList
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        deleteT={deleteT}
+        editT={editT}
+      />
+    )
+  );
   return (
-
-    <Container>
     <div className="App">
       <Header />
       <div className="text-field">
         {" "}
-        <Input />
+        <Input addTask={addTask} />
       </div>
       <div className="todo-list">
         {" "}
-        <TodoList />
+        <ul>{taskList}</ul>
       </div>
     </div>
-    </Container>
   );
 }
 
