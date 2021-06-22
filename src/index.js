bcbc58f34e3df18ffdd2596c9f09175db406f7ae
Jwynd1 <57572152+Jwynd1@users.noreply.github.com>
2021-06-22 00:50:33 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+const DATA = [
+  { id: "todo-0", name: "hi", completed: true },
+];
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App  tasks={DATA}/>
   </React.StrictMode>,
   document.getElementById("root")
 );
