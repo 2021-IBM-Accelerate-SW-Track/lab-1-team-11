c62e0653a98f81ebad2c5831c10f93d91dd47f6f
Jwynd1 <57572152+Jwynd1@users.noreply.github.com>
2021-06-22 00:49:05 -0400

    Update todo.js

--- src/component/todo/todo.js
+++ src/component/todo/todo.js
@@ -1,14 +1,50 @@
 import React, { useState } from "react";
 import { Container, Icon } from "@material-ui/core";
 import Checkbox from "@material-ui/core/Checkbox";
-import { Delete } from "@material-ui/icons";
-import Modal from "../modal";
+import { Delete, PinDropSharp } from "@material-ui/icons";
+//import Modal from "../modal";
 import "./todo.css";
+function Modal({children, shown, close, editT, id, props}) {
+  const [newName, setNewName] = useState('');
+  function handleChange(e) {
+    setNewName(e.target.value);
+  }
+  function handleSubmit(e) {
+    editT(id, newName);
+    setNewName("");
+  }
+  
+  return shown ? (
+    <div
+      className="modal-backdrop"
+      onClick={() => {
+        // close modal when outside of modal is clicked
+        close();
+      }}
+    >
 
-export default function TodoList() {
+      <div
+        className="modal-content"
+        onClick={e => {
+          // do not close modal if anything inside modal content is clicked
+          e.stopPropagation();
+        }}
+      >
+        <input id={id} type="text" name="Newname" value={newName}
+        onChange={handleChange} />
+        <button onClick={close}>Close</button>
+        <button onClick={function(event){ close(); handleSubmit()}}>Save Changes</button>
+        {children}
+
+      </div>
+    </div>
+  ) : null;
+}
+export default function TodoList(props) {
   const [show, setShow] = useState(false);
+  const [isEditing, setEditing] = useState(false);
   const [checked, setChecked] = React.useState(false);
-
+  const [modalShown, toggleModal] = React.useState(false);
   const handleChange = (event) => {
     setChecked(event.target.checked);
   };
@@ -18,18 +54,32 @@ export default function TodoList() {
 
   return (
     <div>
-      <Container>
-        <Checkbox
-          checked={checked}
-          onChange={handleChange}
-          inputProps={{ "aria-label": "primary checkbox" }}
-        />
-        <Icon id="editBtn" onClick={handleShow}>
+      <div className="c-cb">
+        <input id="{props.id}" type="checkbox" defaultChecked={props.completed} />
+        <label className="todo-label">
+          {props.name}
+        </label>
+      </div>
+      <div className="btn-group">
+      <Icon id="editBtn" onClick={() => {
+          toggleModal(!modalShown);
+        }}>
           edit_icon
         </Icon>
-        <Delete id="deleteBtn" />
-        <Modal show={show} />
-      </Container>
+        <Delete id="deleteBtn" onClick={() => props.deleteT(props.id)} />
+        <Modal
+        shown={modalShown}
+        close={() => {
+          toggleModal(false);
+        } } 
+        editT={props.editT}
+        id={props.id}
+        name={props.name}
+        completed={props.completed}
+        key={props.id}
+      >
+      </Modal>
+      </div>
     </div>
   );
 }
